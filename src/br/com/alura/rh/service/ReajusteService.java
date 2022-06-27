887fd8541f1ed1dc2161ebec20bd9eced1c4800b
dvr.rafa@gmail.com <dvr.rafa@gmail.com>
2022-06-27 13:07:20 -0300

    Open Closed Principle - Aberta para extensão, fechada para moridicação

--- src/br/com/alura/rh/service/ReajusteService.java
+++ src/br/com/alura/rh/service/ReajusteService.java
@@ -1,30 +1,21 @@
 package br.com.alura.rh.service;
 
 import java.math.BigDecimal;
-import java.math.RoundingMode;
-import java.time.LocalDate;
-import java.time.temporal.ChronoUnit;
+import java.util.List;
 
-import br.com.alura.rh.ValidacaoException;
 import br.com.alura.rh.model.Funcionario;
 
 public class ReajusteService {
+	private List<ValidacaoReajuste> validacoes;
 	
+	public ReajusteService(List<ValidacaoReajuste> validacoes) {
+		super();
+		this.validacoes = validacoes;
+	}
+
 	public void reajustarSalarioDoFuncionario(Funcionario funcionario, BigDecimal aumento) {
-		BigDecimal salarioAtual = funcionario.getSalario();
-		BigDecimal percentualReajuste = aumento.divide(salarioAtual, RoundingMode.HALF_UP);
-		if (percentualReajuste.compareTo(new BigDecimal("0.4")) > 0) {
-			throw new ValidacaoException("Reajuste nao pode ser superior a 40% do salario!");
-		}
-		LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
-		LocalDate dataAtual = LocalDate.now();
-		long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
-		
-		if (mesesDesdeUltimoReajuste < 6) {
-			throw new ValidacaoException("Intervalor entre reajustes deve ser de no míninmo 6 meses");
-		}
-		
-		BigDecimal salarioReajustado = salarioAtual.add(aumento);
+		this.validacoes.forEach(v -> v.validar(funcionario, aumento));		
+		BigDecimal salarioReajustado = funcionario.getSalario().add(aumento);
 		funcionario.atualizarSalario(salarioReajustado);
 		
 	}
---
+++ src/br/com/alura/rh/service/ValidacaoPeriodicidadeEntreReajustes.java
@@ -0,0 +1,23 @@
+package br.com.alura.rh.service;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+import java.time.LocalDate;
+import java.time.temporal.ChronoUnit;
+
+import br.com.alura.rh.ValidacaoException;
+import br.com.alura.rh.model.Funcionario;
+
+public class ValidacaoPeriodicidadeEntreReajustes implements ValidacaoReajuste{
+	public void validar(Funcionario funcionario, BigDecimal aumento) {
+		LocalDate dataUltimoReajuste = funcionario.getDataUltimoReajuste();
+		LocalDate dataAtual = LocalDate.now();
+		long mesesDesdeUltimoReajuste = ChronoUnit.MONTHS.between(dataUltimoReajuste, dataAtual);
+		
+		if (mesesDesdeUltimoReajuste < 6) {
+			throw new ValidacaoException("Intervalor entre reajustes deve ser de no míninmo 6 meses");
+		}
+		
+	}
+
+}
